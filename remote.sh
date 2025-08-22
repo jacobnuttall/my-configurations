58947a5f5f2ac5ce76797dbca9d792db98f3d5c1
Jacob Nuttall <jacob_nuttall@outlook.com>
2025-08-22 17:21:42 -0600

    Update remote.sh mount point.

--- remote.sh
+++ remote.sh
@@ -3,12 +3,21 @@
 #TODO: Define a function to show available remote services.
 
 source ~/scripts/colors.sh
+
+
+mount_point() {
+    remote=$1
+    echo $HOME/mnt/$remote
+}
+
+[[ ! -d "$(mount_point )" ]] && mkdir $(mount_point)
+
 start_mount() {
     remote=$1
-    if [[ ! -d "$HOME/$remote" ]]; then 
+    if [[ ! -d "$(mount_point $remote)" ]]; then 
         echo -e "$BOLD${GREEN}Mounting remote service $1:$RESET"
-        mkdir $HOME/$remote 
-        rclone mount $remote: ~/$remote \
+        mkdir $(mount_point $remote) 
+        rclone mount $remote: $(mount_point $remote) \
             --vfs-cache-mode=full        \
             --daemon                    \
             --vfs-cache-max-size 10G    \
@@ -19,11 +28,11 @@ start_mount() {
 stop_mount() {
     remote=$1
 
-    if [[ -d "$HOME/$remote" ]]; then 
+    if [[ -d "$(mount_point $remote)" ]]; then 
         echo -e "$BOLD${RED}Unmounting remote service $1:$RESET"
         kill -SIGTERM $( ps aux | grep $remote: | grep rclone | awk '{ print $2 }')
         sleep 2
-        umount $HOME/$remote
-        rmdir $HOME/$remote
+        umount $(mount_point $remote)
+        rmdir $(mount_point $remote)
     fi
 }
