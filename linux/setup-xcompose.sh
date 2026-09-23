9acb07e96e8828606375a2cd79bbded9f4d3629d
Jacob <jacob_nuttall@outlook.com>
2026-09-22 22:26:51 -0600

    Improved robustness of setup-xcompose by accounting for an already-existing .XCompose file.

--- linux/setup-xcompose.sh
+++ linux/setup-xcompose.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+
+prompt="y"
+[ -a ~/.XCompose ] && read -r -p "Overwrite existing .XCompose file? [y/N] " prompt
+if [ "${prompt@L}" == "y" ]; then
+rm -fv ~/.XCompose
+rm -fv ~/.config/xcompose
+else
+echo "Gracefully exiting without overwriting/deleting existing .XCompose file."
+fi	
+
 ln -s "$(pwd)/dotfiles/home/.XCompose" $HOME
 ln -s "$(pwd)/dotfiles/xcompose/" $HOME/.config
 ibus restart
+
+echo "Made symbolic links to .XCompose files."
 
