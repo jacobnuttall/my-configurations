facafec0cd1b7da19cf07f2ccea3d643bf8667cd
Jacob <jacob_nuttall@outlook.com>
2026-09-23 00:17:22 -0600

    Updated setup-scripts.sh with small fix to check for broken links.

--- linux/setup-scripts.sh
+++ linux/setup-scripts.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
+
+
 mkdir -p $HOME/.scripts
-ln -s $(pwd)/scripts/colors.sh $HOME/.scripts
-ln -s $(pwd)/scripts/remote.sh $HOME/.scripts
+
+prompt="y"
+[ -a ~/.scripts/colors.sh ] && read -r -p "Overwrite existing colors.sh file? [y/N] " prompt
+if [ "${prompt@L}" == "y" ]; then
+rm -fv ~/.colors.sh
+else
+echo "Gracefully exiting without overwriting/deleting existing colors.sh file."
+fi	
+
+if [ ! -a ~/.scripts/colors.sh ] ; then
+ln -s "$(pwd)/scripts/colors.sh" $HOME/.scripts/colors.sh
+echo "Made symbolic links to colors.sh file."
+fi 
+
+[ -a ~/.scripts/remote.sh ] && read -r -p "Overwrite existing remote.sh file? [y/N] " prompt
+if [ "${prompt@L}" == "y" ]; then
+rm -fv ~/.remote.sh
+else
+echo "Gracefully exiting without overwriting/deleting existing remote.sh file."
+fi	
+
+if [ ! -a ~/.scripts/remote.sh ] ; then
+ln -s "$(pwd)/scripts/remote.sh" $HOME/.scripts/remote.sh
+echo "Made symbolic links to remote.sh file."
+fi 
